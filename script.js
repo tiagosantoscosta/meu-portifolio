0b23c3efe42f3fe31a617628a6893b3d70777ab5
Tiago Santos Costa <tiagocostta@outlook.com>
2022-11-24 17:44:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,12 @@ const animationClass = 'animate';
 function animeScroll () {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
     target.forEach(function(element) {
-        if ((windowTop) > element.offsetTop)
-        element.classList.add(animationClass)
+        if ((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+        } else { 
+            element.classList.remove(animationClass);
+
+        }
     })
 }
 
