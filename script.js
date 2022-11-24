38e0c200ba06cc0393a562e540c57f5c47922528
Tiago Santos Costa <tiagocostta@outlook.com>
2022-11-24 17:33:30 -0300

    Up

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function typeWriter(el) {
 typeWriter(elementEl)
 
 
+
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll () {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if ((windowTop) > element.offsetTop)
+        element.classList.add(animationClass)
+    })
+}
+
+window.addEventListener('scroll', function () {
+    animeScroll();
+})
